89b1b21f8673129a38a9d6a428d9c24808122ed5
HHamana <hadrien@hamana.fr>
2019-08-20 03:27:47 +0900

    random seed generator

---
+++ src/seeds.rs
@@ -0,0 +1,36 @@
+use super::grid;
+use rand::Rng;
+
+pub fn random_seed() -> Vec<Vec<grid::Cell>> {
+    let dimensions = rand::thread_rng().gen_range(0, 65);
+    let mut seed = Vec::new();
+    for x in 0..dimensions {
+        let mut column = Vec::new();
+        for y in 0..dimensions {
+            let cell = grid::Cell::new(x, y, rand::thread_rng().gen_range(0, 2) >= 1);
+            column.push(cell)
+        }
+        seed.push(column);
+    }
+    seed
+}
+
+mod test {
+    // use super::random_seed;
+
+    #[test]
+    fn non_random_random_seed() {
+        let dimensions = 3;
+        let mut seed = Vec::new();
+        for x in 0..dimensions {
+            let mut column = Vec::new();
+            for y in 0..dimensions {
+                let cell = grid::Cell::new(x, y, false);
+                column.push(cell)
+            }
+            seed.push(column);
+        };
+        assert_eq!(seed[0][0].alive, false);
+        assert_eq!(seed[2][2].alive, false);
+}
+} 
