f70e01e4bacd3f4637b2a8aa039b508cc1489677
HHamana <hadrien@hamana.fr>
2019-08-21 01:49:27 +0900

    clean comments

--- src/main.rs
+++ src/main.rs
@@ -6,36 +6,28 @@ use std::time::{Instant, Duration};
 use std::sync::mpsc;
 
 const DELAY : u64 = 23;
-
 fn main() {
     let now = Instant::now();
 
-    // Here we go
-    let iterations = 500;
+    // Sender/Receiver pattern on 2 threads
 	let (tx, rx) = mpsc::channel();
-    // for i in 0..iterations {
     let txclone = mpsc::Sender::clone(&tx);
 
+    // Sender thread generates ticks
     thread::spawn(move || {
         let seed = seeds::random_seed();
         let mut old_grid = grid::Grid::new_from_seed(seed);
-        for _i in 0..iterations {
+        loop {
             let next_grid = old_grid.tick();
             old_grid = next_grid.tick();
             txclone.send(next_grid).unwrap();
             thread::sleep(Duration::from_millis(DELAY));
         }
     });
-
+    // Receiver (also main) thread draws
     for received in rx {
         print!("\x1B[2J");
 		draw::to_sysout(received);
-        println!("{:?}", now.elapsed());
+        println!("{:?} elapsed. Press Ctrl+C or close the terminal to kill the game.", now.elapsed());
     }
-
-    // while iterations < 10 {
-    //     iterations += 1;
-    //     let grid = &grid.tick();
-    // }
-    
 }
